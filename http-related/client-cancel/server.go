c67376bcd37cc66a10cae89d8d0ee806c8ca1391
Jim Lin <b04705003@ntu.edu.tw>
2020-11-07 14:48:49 +0800

    add server handle client cancellation

---
+++ http-related/client-cancel/server.go
@@ -0,0 +1,26 @@
+package main
+import (
+    "fmt"
+    "net/http"
+    "time"
+    "log"
+)
+
+func main(){
+    http.HandleFunc("/", handler)
+    http.ListenAndServe("127.0.0.1:8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+    ctx := r.Context()
+    log.Printf("handler started")
+    defer log.Printf("handler ended")
+    select{
+    case <-time.After(5 * time.Second):
+        fmt.Fprintln(w, "hello")
+    case <- ctx.Done():
+        err := ctx.Err()
+        log.Print(err)
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
